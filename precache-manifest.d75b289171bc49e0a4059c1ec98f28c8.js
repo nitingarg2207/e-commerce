570ba4715b1fdef6cb59ba083caf6739d193b999
Nitin Garg <nitin1960.be20@chitkara.edu.in>
2023-04-28 02:18:44 +0530

    Updates

---
+++ precache-manifest.d75b289171bc49e0a4059c1ec98f28c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "55436671a4e628e71462",
+    "url": "/e-commerce/static/css/main.0b5ce0b0.chunk.css"
+  },
+  {
+    "revision": "55436671a4e628e71462",
+    "url": "/e-commerce/static/js/main.55436671.chunk.js"
+  },
+  {
+    "revision": "e68b7c363c4ae7f97331",
+    "url": "/e-commerce/static/css/1.a0e63fb1.chunk.css"
+  },
+  {
+    "revision": "e68b7c363c4ae7f97331",
+    "url": "/e-commerce/static/js/1.e68b7c36.chunk.js"
+  },
+  {
+    "revision": "88af3fcb8e26f5b1ba78",
+    "url": "/e-commerce/static/js/runtime~main.88af3fcb.js"
+  },
+  {
+    "revision": "7954e5c29631f6a0493e91b5ee890a01",
+    "url": "/e-commerce/static/media/logo.7954e5c2.png"
+  },
+  {
+    "revision": "469eae847c7273531e240cccc1b5f56f",
+    "url": "/e-commerce/static/media/email.469eae84.svg"
+  },
+  {
+    "revision": "220ab2764d8d5aec49b0ed03b5cfb200",
+    "url": "/e-commerce/static/media/location.220ab276.svg"
+  },
+  {
+    "revision": "b8e883e4295fb2b450c77b57ddacb334",
+    "url": "/e-commerce/static/media/phone.b8e883e4.svg"
+  },
+  {
+    "revision": "3e07995819e8ec733244e933043341f4",
+    "url": "/e-commerce/index.html"
+  }
+];
